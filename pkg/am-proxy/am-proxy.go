2bfeddff93492cd702e73fc87623dc55b5e8a327
theprimeagen <theprimeagen@gmail.com>
2024-10-15 10:45:12 -0600

    simple sim test completed

--- pkg/am-proxy/am-proxy.go
+++ pkg/am-proxy/am-proxy.go
@@ -14,27 +14,30 @@ import (
 var AMProxyDisallowed = fmt.Errorf("unable to connnect, please try again later")
 
 type AMConnectionWrapper struct {
-	conn           AMConnection
-	gameServerConn AMConnection
+	cConn AMConnection
+	gConn AMConnection
 
 	ctx    context.Context
 	cancel context.CancelFunc
 	idx    int
+
+	cFramer packet.PacketFramer
+	gFramer packet.PacketFramer
 }
 
 func (a *AMConnectionWrapper) Close() error {
-    a.cancel()
-    a.conn.Close()
-    if a.gameServerConn != nil {
-        a.gameServerConn.Close()
-    }
-    return nil
+	a.cancel()
+	a.cConn.Close()
+	if a.gConn != nil {
+		a.gConn.Close()
+	}
+	return nil
 }
 
 type AMProxy struct {
 	servers GameServer
 	match   *MatchMakingServer
-    factory ConnectionFactory
+	factory ConnectionFactory
 
 	logger      *slog.Logger
 	connections []*AMConnectionWrapper
@@ -48,7 +51,7 @@ func NewAMProxy(ctx context.Context, servers GameServer, factory ConnectionFacto
 	return AMProxy{
 		servers: servers,
 		match:   NewMatchMakingServer(servers),
-        factory: factory,
+		factory: factory,
 
 		logger:      slog.Default().With("area", "AMProxy"),
 		connections: []*AMConnectionWrapper{},
@@ -73,7 +76,7 @@ func (m *AMProxy) Add(conn AMConnection) error {
 
 	ctx, cancel := context.WithCancel(m.ctx)
 	wrapper := &AMConnectionWrapper{
-		conn:   conn,
+		cConn:  conn,
 		ctx:    ctx,
 		cancel: cancel,
 	}
@@ -105,61 +108,81 @@ func (m *AMProxy) removeConnection(w *AMConnectionWrapper, report error) {
 
 	if report != nil {
 		pkt := packet.CreateErrorPacket(report)
-		_, err := pkt.Into(w.conn)
+		_, err := pkt.Into(w.cConn)
 		if err != nil {
 			m.logger.Error("could not write error message into connection", "error", err)
 		}
 	}
 
+    w.Close()
+
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
 
 	//m.connections = append(m.connections[:w.idx], m.connections[w.idx+1:]...)
-    m.connections[w.idx] = nil
-    m.open = append(m.open, w.idx)
+	m.connections[w.idx] = nil
+	m.open = append(m.open, w.idx)
 }
 
 func (m *AMProxy) handleConnection(w *AMConnectionWrapper) {
-	authPacket, err := packet.ReadOne(w.conn)
-	if err != nil {
-		m.removeConnection(w, err)
+	w.cFramer = packet.NewPacketFramer()
+    w.gFramer = packet.NewPacketFramer()
+    go packet.FrameWithReader(&w.cFramer, w.cConn)
+
+    // TODO(v1) this could hang forever and dumb brown hat hackers could hurt
+    // my delicious ports and memory :(
+	authPacket, ok := <-w.cFramer.C
+
+	if !ok || authPacket == nil {
+		m.removeConnection(w, nil)
 		return
 	}
 
 	// TODO i probably want to have a "user" object that i can
 	// serialize/deserialize
-	if err = m.authenticate(authPacket); err != nil {
+    if err := m.authenticate(authPacket); err != nil {
 		m.removeConnection(w, err)
 		return
 	}
 
-	gameConnStr, err := m.match.matchmake(m.ctx, w.conn)
+	gameConnStr, err := m.match.matchmake(m.ctx, w.cConn)
 	if err != nil {
 		m.removeConnection(w, err)
 		return
 	}
 
-    gameConn, err := m.factory(gameConnStr)
+	gameConn, err := m.factory(gameConnStr)
 	if err != nil {
 		m.removeConnection(w, err)
 		return
 	}
 
-    w.gameServerConn = gameConn
+	w.gConn = gameConn
+    go packet.FrameWithReader(&w.gFramer, w.gConn)
 
-    resp := packet.CreateServerAuthResponse(true)
-    _, err = resp.Into(w.conn)
+	resp := packet.CreateServerAuthResponse(true)
+	_, err = resp.Into(w.cConn)
 	if err != nil {
 		m.removeConnection(w, err)
 		return
 	}
 
-    // TODO i probably need to handle a whole set of conditions here...
-    // i mean i don't even forward out errors from one side to the other
-    // there is also going to be a close
-    // holy cow... so much stuff
-    go io.Copy(w.gameServerConn, w.conn)
-    go io.Copy(w.conn, w.gameServerConn)
+	go m.handleConnection(w)
+}
+
+func (m *AMProxy) handleConnectionLifecycles(w AMConnectionWrapper) {
+	// TODO handle the lifecycles
+	// turn client and server into framers and handle the closing edge cases
+	// and context shutting off
+    // already have the framers by this point, so just intercept the packets
+    // and check for any control plane packets
+	go func() {
+		io.Copy(w.gConn, w.cConn)
+	}()
+
+	go func() {
+		io.Copy(w.cConn, w.gConn)
+	}()
 }
 
 func (m *AMProxy) Close() {
@@ -169,7 +192,7 @@ func (m *AMProxy) Close() {
 
 	m.closed = true
 	for _, c := range m.connections {
-        c.Close()
+		c.Close()
 	}
 
 	m.connections = []*AMConnectionWrapper{}
